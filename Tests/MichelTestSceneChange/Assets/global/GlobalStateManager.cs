1efaf5cfa9f2cb3c13f7cf7811e55c096b54f30f
SoullessStone <micheldc@windowslive.com>
2016-09-30 19:51:21 +0200

    Globaler Buchhalter implementiert, closes #4

---
+++ Tests/MichelTestSceneChange/Assets/global/GlobalStateManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GlobalStateManager :  MonoBehaviour {
+	public static Dictionary<int, bool> levels = new Dictionary<int, bool> ();
+
+	// Use this for initialization
+	void Start () {
+		// This object will live forever! FOREVER!!!
+		DontDestroyOnLoad(this);
+
+		// Add some levels
+		levels.Add (1, true);
+		levels.Add (2, false);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		bool value;
+		Debug.Log ("Ha! Ich lebe noch");
+		GlobalStateManager.levels.TryGetValue (1, out value);
+		Debug.Log ("Level 1 bestanden? " + value);
+		GlobalStateManager.levels.TryGetValue (2, out value);
+		Debug.Log ("Level 2 bestanden? " + value);
+	}
+}
--- Tests/MichelTestSceneChange/Assets/scene1/change.cs
+++ Tests/MichelTestSceneChange/Assets/scene1/change.cs
@@ -3,16 +3,15 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class change : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyUp(KeyCode.A)){
+		if (Input.GetKeyUp(KeyCode.Space)){
 			SceneManager.LoadScene(1);
+			GlobalStateManager.levels [2] = true;
 		}
+		// This accesses a state from the undestroyable GlobalStateManager
+		bool value;
+		GlobalStateManager.levels.TryGetValue (2, out value);
 	}
 }

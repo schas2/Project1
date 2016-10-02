213fefc4bc34dee7d39885665554e6e44dd6e844
SoullessStone <micheldc@windowslive.com>
2016-10-02 11:16:44 +0200

    Communication between objects example

---
+++ Tests/MichelTestCommunicationBetweenObjects/Assets/ActionListener.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionListener : MonoBehaviour {
+
+	public void ichRufeDich() {
+		Destroy (this.gameObject);
+	}
+
+	public void ichRufeDichMitParam(bool param) {
+		Debug.Log ("Ich höre dich mit param: " + param);
+	}
+}
---
+++ Tests/MichelTestCommunicationBetweenObjects/Assets/CallCylinder.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CallCylinder : MonoBehaviour {
+	public GameObject otherObject;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp(KeyCode.Space)){
+			Debug.Log ("click");
+			otherObject.SendMessage ("ichRufeDich");
+			otherObject.SendMessage ("ichRufeDichMitParam", true);
+		}
+	}
+}

5959070e959fa7f446ddf68416dae5d91ae4f182
SoullessStone <micheldc@windowslive.com>
2016-10-19 23:06:19 +0200

    Mit Labyrinth begonnen, #14

---
+++ Rooms/Room_2_Labyrinth/Assets/room_2/scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private NavMeshAgent navMeshAgent;
+
+    void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                navMeshAgent.destination = hit.point;
+                navMeshAgent.Resume();
+            }
+        }
+    }
+}

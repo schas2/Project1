294a910178304bf7ec05dc139ba30d35af474bd7
SoullessStone <micheldc@windowslive.com>
2016-11-11 11:43:43 +0100

    removed unused assets in labyrinth, started main project

---
+++ MainProject/ProjectEscape/Assets/Rooms/room_2/scripts/ExitDoor.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ExitDoor : MonoBehaviour {
+
+	// Use this for initialization
+	void OnMouseDown () {
+		SceneManager.LoadScene (1);
+	}
+}
---
+++ MainProject/ProjectEscape/Assets/Rooms/room_2/scripts/MoveTo.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTo : MonoBehaviour {
+	NavMeshAgent navMeshAgent;
+	void Start()
+	{
+		navMeshAgent = GetComponent<NavMeshAgent>();
+	}
+
+	void Update()
+	{
+		Camera cam = Camera.allCameras [0];
+		Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Input.GetButtonDown("Fire1"))
+		{
+			if (Physics.Raycast(ray, out hit, 100))
+			{
+				navMeshAgent.destination = hit.point;
+				navMeshAgent.Resume();
+			}
+		}
+	}
+}

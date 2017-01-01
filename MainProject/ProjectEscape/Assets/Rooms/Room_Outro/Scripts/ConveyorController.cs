112706934ca5adc29b9ac83000e4b36608bff58b
schas2 <schas2@nomail.com>
2017-01-01 19:35:59 +0100

    "First draft" Outro erstellt

---
+++ MainProject/ProjectEscape/Assets/Rooms/Room_Outro/Scripts/ConveyorController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConveyorController : MonoBehaviour
+{
+	public float speed;
+
+	void Update() {
+		float zPosition = transform.position.z;
+
+		transform.position = new Vector3(transform.position.x, transform.position.y, zPosition + speed);
+	}
+}
---
+++ MainProject/ProjectEscape/Assets/Rooms/Room_Outro/Scripts/HammerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HammerController : MonoBehaviour
+{
+	public float speed;
+
+	private int direction = -1;
+
+	void Update() {
+		float yPosition = transform.position.y;
+
+		if (yPosition <= 5 || yPosition >= 14) { 
+			direction *= -1;
+		}
+
+		transform.position = new Vector3(transform.position.x, yPosition + direction * speed, transform.position.z);
+	}
+}

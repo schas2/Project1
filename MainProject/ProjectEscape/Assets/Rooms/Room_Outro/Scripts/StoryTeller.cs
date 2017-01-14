d1f3e8c06ef4773c600944a50c3fb7c0e6ad11a4
schas2 <schas2@nomail.com>
2017-01-14 17:47:26 +0100

    "Good Ending" erstellt

---
+++ MainProject/ProjectEscape/Assets/Rooms/Room_Outro/Scripts/StoryTeller.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoryTeller : MonoBehaviour
+{
+	public Camera cameraGoodEnd;
+	public Camera cameraBadEnd;
+
+	void Awake() {
+		int score = 62; //GameMemory.getScore();
+		if (score < 50) {
+			cameraGoodEnd.enabled = false;
+			cameraBadEnd.enabled = true;
+		} else {
+			cameraGoodEnd.enabled = true;
+			cameraBadEnd.enabled = false;
+		}
+	}
+}

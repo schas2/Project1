cf0430294ba4ced37a5d9eb3e581199857a8f485
schas2 <schas2@nomail.com>
2017-01-18 19:23:51 +0100

    Outro Text erstellt

--- MainProject/ProjectEscape/Assets/Rooms/Room_Outro/Scripts/StoryTeller.cs
+++ MainProject/ProjectEscape/Assets/Rooms/Room_Outro/Scripts/StoryTeller.cs
@@ -1,10 +1,14 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class StoryTeller : MonoBehaviour
 {
 	public Camera cameraGoodEnd;
 	public Camera cameraBadEnd;
+
+	public Text playerNotification;
+	public GameObject ui;
 
 	public NavigationManager naviManager;
 
@@ -15,9 +19,12 @@ public class StoryTeller : MonoBehaviour
 		if (score < 70) {
 			cameraGoodEnd.enabled = false;
 			cameraBadEnd.enabled = true;
+			ui.SetActive(false);
 		} else {
 			cameraGoodEnd.enabled = true;
 			cameraBadEnd.enabled = false;
+			ui.SetActive(true);
+			playerNotification.text = "Wissenschaftler Herbert: Zwei Ferienwochen extra!!!\nDanke du bist super.";
 		}
 
 		if (!(GameMemory.getOutroState() is LevelCompleted)) {
@@ -35,6 +42,8 @@ public class StoryTeller : MonoBehaviour
 
 	public void endGame() {
 		GameMemory.setOutroState(new FinishedOutro());
+		GameMemory.save();
+
 		StartCoroutine(naviManager.goMainScene(5));
 	}
 }
